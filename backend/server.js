9be0d3586798eefaa8700d15afe005a31c49533d
Muhammad Saad Saif <gcsaadsaif123@gmail.com>
2026-02-10 22:40:35 +0500

    makin backned for all frontend origin

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "*",
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
+    allowedHeaders: ["Content-Type", "auth-token"],
   }),
 );
 
